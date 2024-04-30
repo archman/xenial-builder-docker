0697874074230678208ebea2de9740358415e44d
Tong Zhang <zhangt@frib.msu.edu>
2024-04-30 14:06:26 -0400

    REF: Update script for building a qt project.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
-QT_DEPLOYER_PATH=/tmp/squashfs-root/AppRun
+set -Eeuo pipefail
+trap _fallback SIGINT SIGTERM ERR EXIT
+trap _on_exit EXIT
+
+_fallback() {
+    trap - SIGINT SIGTERM ERR
+    echo "An error occurred for auto-build. Falling back to interactive shell..."
+    if [ -t 0 ]; then
+        /bin/bash
+    else
+        echo "Not running interactively. Exiting..."
+        exit 1
+    fi
+}
+
+_on_exit() {
+    trap - EXIT
+    echo "Cleaning up before exiting..."
+    rm -rf ${dist_dir}
+    make distclean
+}
+
 QT_DEPLOYER_OPTS="-bundle-non-qt-libs -no-translations"
-MAKESELF_PATH=makeself
 MAKESELF_OPTS="--notemp --nox11"
-QMAKE=/usr/lib/qt-new/bin/qmake
 MAXNJOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
-PRO_NAME="LISE_Package.pro"
-EXECS="LISE++ Charge Gemini PACE4 Global KanteleHandbook"
 
 njobs=''
 ver=''
+pro_file=''
+execs=''
+dist_dir=''
+app_name=''
 while :; do
     case "${1-}" in
         -j | --jobs)
             njobs="${2-}"
+            shift
+            ;;
+        -p | --project)
+            pro_file="${2-}"
+            shift
+            ;;
+        -e | --executables)
+            execs="${2-}"
+            shift
+            ;;
+        --app)
+            app_name="${2-}"
+            shift
+            ;;
+        -d | --dist)
+            dist_dir="${2-}"
             shift
             ;;
         --version)
@@ -27,48 +64,63 @@ while :; do
     shift
 done
 if [[ -z "$ver" ]]; then
-    echo "Missing required parameter: --version"
+    echo "Missing required parameter for package version: --version"
+    exit 1
+fi
+if [[ -z "${pro_file}" ]]; then
+    echo "Missing required parameter for project file: -p | --project"
+    exit 1
+fi
+if [[ -z "$execs" ]]; then
+    echo "Missing required parameter for executables: -e | --executables"
     exit 1
 fi
 if [[ -z ${njobs} || ${njobs} -gt ${MAXNJOBS} ]]; then
     njobs=${MAXNJOBS}
 fi
+if [[ -z "${dist_dir}" ]]; then
+    dist_dir="dist"
+fi
+if [[ -z "${app_name}" ]]; then
+    app_name="qt-app"
+fi
 
-cd /appbuilder
-[[ ! -e ${PRO_NAME} ]] && bash -i
+echo "NJobs: $njobs"
+echo "Version: $ver"
+echo "Project: $pro_file"
+echo "Executables: $execs"
+echo "Dist dir: $dist_dir"
+echo "App Name: $app_name"
+
+# main executable
+exec0=$(echo ${execs} | cut -d' ' -f1)
 
 # building
-${QMAKE} CONFIG+=release CONFIG+=optimize_full ${PRO_NAME}
+cd /appbuilder
+qmake CONFIG+=release CONFIG+=optimize_full ${pro_file}
 make -j${njobs}
 
 # bundling
-cp -r ${EXECS} _install
-rm $(find _install -iname '*.dll' -o -iname '*.exe' | xargs)
-find _install -empty -type d -delete
-
-mv _install lise-app
-cd lise-app
-for app in ${EXECS}
+[[ ! -e ${dist_dir} ]] && mkdir -p ${dist_dir}
+cp -r ${execs} ${dist_dir}
+cd ${dist_dir}
+for app in ${execs}
 do
-    ${QT_DEPLOYER_PATH} ${app} -qmake=${QMAKE} ${QT_DEPLOYER_OPTS}
+    linuxdeployqt ${app} -qmake=qmake ${QT_DEPLOYER_OPTS}
     # find . -iname '*.so' -exec strip --strip-unneeded {} \;
     strip --strip-unneeded ${app}
 done
 
-cat << EOF > run_lise.sh
+cat << EOF > run_app.sh
 #!/bin/sh
 cwdir=\`dirname \$0\`
-\${cwdir}/LISE++
+\${cwdir}/${exec0}
 EOF
-chmod +x run_lise.sh
-
-cp lisepp.ini lisepp_original.ini
-sed -e '/^size/s/size=.*/size=11/;/^sound/s/sound=.*/sound=0/;/^3d/s/3d.*/3d-animation=0/;/^table/s/table=.*/table=-1/;s/^M$//' lisepp_original.ini  > lisepp.ini
-dos2unix lisepp.ini
+chmod +x run_app.sh
 
 cd ..
-tar cjf /appbuilder/lise-app_${ver}.orig.tar.bz2 lise-app
+tar cjf /appbuilder/${app_name}_${ver}.orig.tar.bz2 ${dist_dir}
 
 # makeself
-${MAKESELF_PATH} ${MAKESELF_OPTS} lise-app /appbuilder/lise_${ver}.run \
-    "LISE++ with Qt Framework" ./run_lise.sh
+makeself ${MAKESELF_OPTS} ${dist_dir} /appbuilder/${app_name}_${ver}.run \
+    "${app_name} with Qt Framework" ./run_app.sh
